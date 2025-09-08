d13586a234c4559dca1c7e9d816730fc35475315
Ahmed ramadan <aedr33635@gmail.com>
2025-09-08 18:58:29 +0300

    Update Restcontroller.java

--- Restcontroller.java
+++ Restcontroller.java
@@ -1,1 +1,17 @@
+import org.sprongframework.web.bind.annotation.*;
+@RestController 
+@RestMapping 
+  public class authcontroller {
+  private final UseRepository userRepo;
+  private final PasswordEncoder encoder;
 
+  public authcontroller (UserRepository userRepo , PasswordEncoder encoder) {
+    this.userRepo = userRepo;
+    this.encoder = encoder;
+    @postMapping("/register")
+      public string register(@requestBody User user) { 
+       user .setPassword(encoder.encode(user.getPassword()));
+       userRepo.save(user); 
+       return "User registered" ; 
+    }
+  }
