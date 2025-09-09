979d900d1eb36c4ef7c590d053e37df4f8a0bb3a
Ahmed ramadan <aedr33635@gmail.com>
2025-09-09 12:20:48 +0300

    Create to_do.java

---
+++ to_do.java
@@ -0,0 +1,22 @@
+import jakarta.persistence.*;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private boolean completed = false;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Todo() {}
+    public Todo(String title, User user) {
+        this.title = title;
+        this.user = user;
+    }
+    
+}
