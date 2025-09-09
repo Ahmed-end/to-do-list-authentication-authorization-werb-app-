5aa833d105a0fde60efef54ff186b34a204c8126
Ahmed ramadan <aedr33635@gmail.com>
2025-09-09 12:18:49 +0300

    Update data_base_table.java

--- data_base_table.java
+++ data_base_table.java
@@ -1,13 +1,19 @@
-import jakarta.presistence.*;
-@Entity 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
-  @Id 
-  @GeneratedValue(strategy = GenerationType.IDETITY)
-  private Long id ; 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(unique = true)
+    private String username;
+    private String password; 
 
- @column(uniuqe = true)
-  private string username ; 
-  private string password ; 
-  private String role = "USER" ; 
+    public User() {}
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
 }
